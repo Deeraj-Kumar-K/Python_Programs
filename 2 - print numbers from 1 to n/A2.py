a208abbe1338e491de831353fb04d34b207f346d
Deeraj Kumar <deerajkmr98@gmail.com>
2021-11-02 18:56:03 +0530

    print no. 1 to n updated

--- 2 - print numbers from 1 to n/A2.py
+++ 2 - print numbers from 1 to n/A2.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
-    n = int(input())
+    n = int(input("Enter a number: "))
     for i in range(1,n+1):
-        print(str(i),end="")
+        #print(str(i),end="")
+        print(i,end="")
