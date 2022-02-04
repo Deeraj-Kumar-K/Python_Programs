82cf3a5d2f253fb2e83664691198a44dc0e52681
Deeraj Kumar <deerajkmr98@gmail.com>
2022-02-04 23:34:51 +0530

    calendar meeting problem with explanation

--- 0 - Algorithms/30 - calendar meetings/calendarMeetings.py
+++ 0 - Algorithms/30 - calendar meetings/calendarMeetings.py
@@ -15,6 +15,9 @@ Times will be given and also should be returned in military time.
 Example time: '9:30', '8:01', '22:55'
 '''
 
+# O(c1 + c2) time , O(c1 + c2) space
+#where c1 and c2 are the length / no. of meetings in calendar1 and calendar2
+
 def calendarMeetings(calendar1, dailyLimits1, calendar2, dailyLimits2, meetingDuration):
     updatedCalendar1 = updateCalendar(calendar1, dailyLimits1)
     updatedCalendar2 = updateCalendar(calendar2, dailyLimits2)
@@ -23,23 +26,23 @@ def calendarMeetings(calendar1, dailyLimits1, calendar2, dailyLimits2, meetingDu
     return gapBetweenMeetings(flattenedCalendar, meetingDuration)
 
 def updateCalendar(calendar, limits):
-    updatedCalendar = calendar[:]
+    updatedCalendar = calendar[:] #creating a copy of calendar by slicing
     updatedCalendar.insert(0, ['0:00', limits[0]])
     updatedCalendar.append([limits[1], '23:59'])
     return list(map(lambda time: [timeToMinutes(time[0]), timeToMinutes(time[1])], updatedCalendar))
 
-def timeToMinutes(time):
-    hours, minutes = list(map(int, time.split(':')))
-    return hours * 60 + minutes
+def timeToMinutes(time): #O(1) time
+    hours, minutes = list(map(int, time.split(':'))) #time is in string type
+    return hours * 60 + minutes #we converted them to minutes(int type)
 
-def mergeCalendar(calendar1, calendar2):
-    merged = []
+def mergeCalendar(calendar1, calendar2): #O(c1 + c2) time, O(c1 + c2) space
+    merged = [] #merging is done in merge sort fashion
     i = 0
     j = 0
     while i < len(calendar1) and j < len(calendar2):
         meeting1 = calendar1[i]
         meeting2 = calendar2[j]
-        if meeting1[0] < meeting2[0]:
+        if meeting1[0] < meeting2[0]: #we are merging by start time of meeting
             merged.append(meeting1)
             i += 1
         else:
@@ -54,11 +57,14 @@ def mergeCalendar(calendar1, calendar2):
     return merged
 
 def flatCalendar(calendar):
-    flat = [calendar[0][:]]
+    #flatenning calendar is combining all the overlapping meetings into one time range
+    #Ex: [['10:00','11:30'], ['10:30','12:00']], these two meetings are overlapping
+    #these can be flattened to one time range ie. [["10:00", "12:00"]]
+    flat = [calendar[0][:]] #initializing with 1st element of calendar by slicing
     for i in range(1, len(calendar)):
         currentMeeting = calendar[i]
         currStart, currEnd = currentMeeting
-        prevMeeting = flat[-1]
+        prevMeeting = flat[-1] #'-1' points to last element in the list
         lastStart, lastEnd = prevMeeting
         if lastEnd >= currStart:
             newMeeting = [lastStart, max(lastEnd, currEnd)]
@@ -68,15 +74,16 @@ def flatCalendar(calendar):
     return flat
     
 def gapBetweenMeetings(calendar, meetingDuration):
-    availableTimeSlots = []
+    availableTimeSlots = [] #stores all time slots when meeting can be scheduled
     for i in range(1, len(calendar)):
-        availableTime = calendar[i][0] - calendar[i - 1][1]
-        if availableTime >= meetingDuration:
+        availableTime = calendar[i][0] - calendar[i - 1][1] #currStart - prevEnd = gap btw both meetings
+        if availableTime >= meetingDuration: #if that time gap is enough to schedule their meeting then,
             availableTimeSlots.append([calendar[i - 1][1], calendar[i][0]])
     return list(map(lambda time: [minutesToTime(time[0]), minutesToTime(time[1])], availableTimeSlots))
 
 def minutesToTime(minutes):
-    hrs = minutes // 60
+    #this function converts minutes(int) to military time(string)
+    hrs = minutes // 60 #floor value is taken
     mins = minutes % 60
     hoursString = str(hrs)
     minutesString = '0' + str(mins) if mins < 10 else str(mins)
