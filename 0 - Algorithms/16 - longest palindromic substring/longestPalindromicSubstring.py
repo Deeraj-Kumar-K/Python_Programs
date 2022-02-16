b47e7a3730d402b868dda5491f07989cd16d99f7
Deeraj Kumar <deerajkmr98@gmail.com>
2022-02-16 20:49:52 +0530

    find longest palindromic substring

--- 0 - Algorithms/16 - longest palindromic substring/longestPalindromicSubstring.py
+++ 0 - Algorithms/16 - longest palindromic substring/longestPalindromicSubstring.py
@@ -10,15 +10,21 @@ Example 2:
 Input: s = "cbbd"
 Output: "bb"
 
-Example 3:
+Example 3: Even a single character is a palindrome.
 Input: s = "a"
 Output: "a"
+
+Example 4: Even a single character is a palindrome.
+Input: s = "abc"
+Output: "a" or "b" or "c"
 '''
 
+# Solution 1:
+
 #Time and Space Complexity
-# Time: O(n) , Space: O(1)
+# Time: O(n^2) , Space: O(1)
 # where n is the length of input string
-    
+'''   
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
@@ -65,3 +71,81 @@ class Solution:
 
 s = "abayyzyyj"
 print(Solution().longestPalindrome(s))
+s = "abc"
+print(Solution().longestPalindrome(s))
+s = "a"
+print(Solution().longestPalindrome(s))
+s = "abaa"
+print(Solution().longestPalindrome(s))
+'''
+
+'''
+# ********* Too much comments, so, a clean version of above code ************
+
+def longestPalindromicSubstring(string):
+    currentLongest = [0, 1]
+    for i in range(1, len(string)):
+        odd = getLongestPalindromeFrom(string, i - 1, i + 1)
+        even = getLongestPalindromeFrom(string, i - 1, i)
+        longest = max(odd, even, key=lambda x: x[1] - x[0])
+        currentLongest = max(longest, currentLongest, key=lambda x: x[1] - x[0])
+    return string[currentLongest[0] : currentLongest[1]]
+
+
+def getLongestPalindromeFrom(string, leftIdx, rightIdx):
+    while leftIdx >= 0 and rightIdx < len(string):
+        if string[leftIdx] != string[rightIdx]:
+            break
+        leftIdx -= 1
+        rightIdx += 1
+    return [leftIdx + 1, rightIdx]
+'''
+
+# Solution 2: It is similiar to above solution
+
+#Time and Space Complexity
+# Time: O(n^2) , Space: O(1)
+
+def longestPalindromicSubstring(string):
+    longest = [0, 0] # stores index values
+    for i in range(1, len(string)):
+        odd = getLongestPalindromeFrom(string, i - 1, i + 1) # Ex: "aba" is odd
+        even = getLongestPalindromeFrom(string, i - 1, i) # "aa" is even
+        currentLongest = getLongestString(odd, even) # longest str of current expansion
+        longest = getLongestString(longest, currentLongest) # longest str of all time
+    return string[longest[0] : longest[1] + 1]
+
+def getLongestPalindromeFrom(string, leftIdx, rightIdx):
+    # while string is in bounds , perform expansion
+    while leftIdx >= 0 and rightIdx < len(string):
+        if string[leftIdx] != string[rightIdx]:
+            break
+        leftIdx -= 1
+        rightIdx += 1
+    return [leftIdx + 1, rightIdx - 1]
+
+def getLongestString(odd, even):
+    lengthOne = odd[1] - odd[0]
+    lengthTwo = even[1] - even[0]
+    if lengthOne > lengthTwo:
+        return odd
+    else:
+        return even
+    
+
+# Input
+string = "a"
+print("String :" ,string)
+print("Longest Palindromic Substring :", longestPalindromicSubstring(string))
+
+string = "abc"
+print("String :" ,string)
+print("Longest Palindromic Substring :", longestPalindromicSubstring(string))
+
+string = "abaa"
+print("String :" ,string)
+print("Longest Palindromic Substring :", longestPalindromicSubstring(string))
+
+string = "abazyxxyz"
+print("String :" ,string)
+print("Longest Palindromic Substring :", longestPalindromicSubstring(string))
